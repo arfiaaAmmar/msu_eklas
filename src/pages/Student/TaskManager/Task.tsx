e204c7cd4ba44bb1ddbf6807b03e4ab6a7dec234
Ammar Hazim <hazimaammar98@gmail.com>
2023-04-18 11:20:31 +0800

    added task component n update package

--- src/pages/Student/TaskManager/Task.tsx
+++ src/pages/Student/TaskManager/Task.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import { CheckBox } from "@mui/icons-material";
+import React from "react";
 
-type Props = {
-    pages:string[];
-    currentPage:string
+type TaskProps = {
+  id: number,
+  title: string,
+  description?: string, 
+  date?: Date,
+  onDelete: Function
 }
 
-const Task = ({pages, currentPage}:Props) => {
+const Task = ({id, title, description, date, onDelete}:TaskProps) => {
   return (
-    <div>Task</div>
-  )
-}
+    <li key={id} className="mx-3 my-2">
+      <div className="flex justify-end w-full p-2 bg-neutral-200 rounded-md">
+        <div className="w-3/4 flex gap-2 ">
+          <div className="bg-red-600 w-1 rounded-sm"></div>
+          <div className="flex flex-col gap-2">
+            <p className="text-lg leading-5 font-semibold">{title}</p>
+            <p className="text-xs">{description}</p>
+            <p className="bg-red-500 text-[0.6rem] p-1 rounded-lg">
+              Assignment
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-end w-1/4 text-right">
+          <CheckBox onClick={() => onDelete(id)} />
+          {date == null ? null : (
+            <p className="text-[0.6rem] font-bold text-neutral-500 mt-2">
+              {date.getDate()}/{date.getMonth() + 1}/
+              {date.getFullYear()}
+            </p>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+};
+
 
 export default Task
