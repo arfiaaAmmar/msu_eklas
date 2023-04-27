4674cd43d861f9d01e43da184fab083d6a34308a
Ammar Hazim <hazimaammar98@gmail.com>
2023-04-27 10:00:05 +0800

    added dummy books

--- src/pages/Campus/Library.tsx
+++ src/pages/Campus/Library.tsx
@@ -1,6 +1,14 @@
 import { QrCodeScanner } from "@mui/icons-material";
 import SearchIcon from "@mui/icons-material/Search";
 
+type Book = {
+  title: string;
+  description: string;
+  publishDate?: Date;
+  availability: Boolean;
+  tags?: "msu" | "science" | "engineering" | undefined;
+};
+
 export default function Library() {
   const books: Book[] = [
     {
@@ -50,14 +58,14 @@ export default function Library() {
         <div className="flex my-4 gap-2">
           <div className="w-4/6">
             <img
-              src="/images/profile_pic.png"
+              src="https://m.media-amazon.com/images/I/71OCKI-99VL.jpg"
               alt=""
               className="object-cover rounded-lg h-full"
             />
           </div>
           <div>
-            <p className="text-lg text-white font-semibold">Book Title.</p>
-            <p className=" text-neutral-300 text-xs">
+            <p className="text-lg text-neutral-50 font-semibold">Book Title.</p>
+            <p className=" text-neutral-50 text-xs">
               Lorem ipsum dolor sit amet cocessitatibus esse? Impedit, at soluta
               sunt magnam ratione voluptates?
             </p>
@@ -67,59 +75,38 @@ export default function Library() {
           </div>
         </div>
         {books.map((book, index) => (
-          <Book
-            title={book.title}
-            description={book.description}
-            tags={book.tags}
-            availability={book.availability}
-          />
+          <div key={index}>
+            <div className="h-[1px] w-full bg-neutral-400 opacity-60" />
+            <div className="flex my-2 gap-2">
+              <div className="w-16">
+                <img
+                  src="https://m.media-amazon.com/images/I/71OCKI-99VL.jpg"
+                  className="rounded-lg object-contain h-full"
+                  alt=""
+                />
+              </div>
+              <div className="text-neutral-50">
+                <p className="font-semibold">{book.title}</p>
+                <p className="text-neutral-50 text-xs">{book.description}</p>
+                <div className="text-[0.7rem] flex gap-2 mt-1">
+                  <p className="bg-blue-500 rounded-md px-[0.2rem]">{book.tags}</p>
+                  <p className="bg-neutral-300 rounded-md px-[0.2rem] text-black">
+                    L3|R4|S5
+                  </p>
+                </div>
+              </div>
+              <div>
+                <p className="text-xs text-neutral-300 text-right">
+                  {book.publishDate?.getFullYear()}
+                </p>
+                <p className="text-white mt-1 bg-red-600 text-xs mr-0 ml-auto rounded-md px-1">
+                  {book.availability == true ? "Available" : "Unavailable"}
+                </p>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </>
   );
 }
-
-type Book = {
-  title: string;
-  description: string;
-  publishDate?: Date;
-  availability: Boolean;
-  tags?: "msu" | "science" | "engineering" | undefined;
-};
-
-import React from "react";
-
-const Book = (book: Book) => {
-  return (
-    <div>
-      <div className="h-[1px] w-full bg-neutral-400 opacity-60" />
-      <div className="flex my-2 gap-2">
-        <div className="w-16">
-          <img
-            src="/images/profile_pic.png"
-            className="rounded-lg object-contain h-full"
-            alt=""
-          />
-        </div>
-        <div className="text-white">
-          <p className="font-semibold">{book.title}</p>
-          <p className="text-xs">{book.description}</p>
-          <div className="text-[0.7rem] flex gap-2 mt-1">
-            <p className="bg-blue-500 rounded-md px-[0.2rem]">{book.tags}</p>
-            <p className="bg-neutral-300 rounded-md px-[0.2rem] text-black">
-              L3|R4|S5
-            </p>
-          </div>
-        </div>
-        <div>
-          <p className="text-xs text-neutral-300 text-right">
-            {book.publishDate?.getFullYear()}
-          </p>
-          <p className="text-white mt-1 bg-red-600 text-xs mr-0 ml-auto rounded-md px-1">
-            {book.availability == true ? "Available" : "Unavailable"}
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
