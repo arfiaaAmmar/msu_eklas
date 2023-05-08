d76b7931a0ee4e3392dd2bbdeafa2081b4195239
Ammar Hazim <hazimaammar98@gmail.com>
2023-05-08 23:25:53 +0800

    Still bugs: closest working progress

--- src/pages/Campus/Booking/Booking.tsx
+++ src/pages/Campus/Booking/Booking.tsx
@@ -1,84 +1,178 @@
-import { useState } from "react";
 import { Button, Tab, Tabs, TextField, Typography } from "@mui/material";
-import { BookingViewer } from "./BookingViewer";
-import TabNav from "../../../components/Navigation/TabNav";
-import { ProfilePic } from "../../../assets/images/images";
+import { useState } from "react";
 import Carousel from "react-material-ui-carousel";
+import { TabContext, TabPanel } from "@mui/lab";
+import BookingTabContent from "./BookingTabContent";
+import { roomsRoute } from "../../../routes/roomsRoute";
+import { RoomBookingRouteType } from "../../../routes/config";
+
+const account = roomsRoute[0]
+const library = roomsRoute[1]
+const counselling = roomsRoute[2]
 
 const Booking = () => {
-  const [page, setPage] = useState("Account");
-  const handleTab = (e: any, newPage: string) => {
-    setPage(newPage);
-  };
+  const [tab, setTab] = useState("account");
+  const [currentRoom, setCurrentRoom] = useState<string | undefined>("account_room1")
+  const carouselImage = account.children?.find(child => child.id === currentRoom)
+  const [booking, setBooking] = useState<RoomBookingRouteType>({
+    id: "account",
+    department: "account",
+    children: account.children,
+    images: account.images,
+  });
 
-  const exampleImages = [ProfilePic, ProfilePic, ProfilePic];
+  const [bookings, setBookings] = useState<RoomBookingRouteType[]>([
+    {
+      id: account.id,
+      department: account.department,
+      images: account.images,
+    },
+    {
+      id: library.id,
+      department: library.department,
+      images: library.images,
+    },
+  ]);
+  // const handleTab = (e: any, newPage: string) => {
+  //   if (booking?.bookingDate == null) return;
+  //   setBoookings([
+  //     ...bookings,
+  //     {
+  //       id: bookings.length + 1,
+  //       description: task?.description,
+  //       date: new Date(),
+  //       flag: "inbox",
+  //       completed: false,
+  //     },
+  //   ]);
+
+  //   setTask({
+  //     id: 0,
+  //     title: "",
+  //     description: "",
+  //     date: new Date(),
+  //     flag: "inbox",
+  //     completed: false,
+  //   });
+  //   setModals({ ...modals, task: !task });
+  // };
+
+  const addBooking = () => {};
+
+  const deleteBooking = () => {};
+
+  const fetchData = (link: URL) => {};
 
   return (
     <>
-      <Typography variant="h6">Book Room</Typography>
-      <div className="bg-neutral-800 text-center pt-0 rounded-lg py-4 shadow-md">
-        <Carousel animation="slide" swipe>
-          {exampleImages.map((image, index) => (
-            <div
-              key={index}
-              className="flex flex-row bg-red-900 rounded-lg h-[20vh] overflow-hidden"
-            >
-              <img
-                className="object-cover w-full"
-                src={image}
-                alt="news picture"
-              />
-            </div>
-          ))}
-        </Carousel>
-        <Tabs
-          value={page}
-          onChange={handleTab}
-          textColor="primary"
-          variant="scrollable"
-          TabIndicatorProps={{
-            style: { backgroundColor: "red"},
-          }}
-        >
-          <Tab value="account" label="Account" className="text-neutral-50 text-[0.8rem]" />
-          <Tab value="library" label="Library" className="text-neutral-50 text-[0.8rem]" />
-          <Tab value="counselling" label="Counselling" className="text-neutral-50 text-[0.8rem]" />
-        </Tabs>
-        <p className="text-neutral-50 font-semibold my-2">Select a Room</p>
-        {[...Array(3)].map((roomNumber, index) => (
-          <Button
-            key={index}
-            className="bg-neutral-200 rounded-md px-2 text-sm mx-1 text-neutral-800"
+      <TabContext value={tab}>
+        <div className="bg-neutral-800 text-center pt-0 rounded-lg py-4 shadow-md">
+          <TabPanel value="account" className="p-0">
+            <Carousel animation="slide" swipe>
+              {
+                carouselImage?.images?.map((image, index) => (
+                  <div
+                  key={index}
+                  className="flex flex-row bg-red-900 rounded-lg h-[20vh] overflow-hidden"
+                >
+                  <img
+                    className="object-cover w-full"
+                    src={image?.toString()}
+                    alt="news picture"
+                  />
+                </div>
+                ))
+              }
+            </Carousel>
+          </TabPanel>
+          <TabPanel value="library" className="p-0">
+            <Carousel animation="slide" swipe>
+            {
+                carouselImage?.images?.map((image, index) => (
+                  <div
+                  key={index}
+                  className="flex flex-row bg-red-900 rounded-lg h-[20vh] overflow-hidden"
+                >
+                  <img
+                    className="object-cover w-full"
+                    src={image?.toString()}
+                    alt="news picture"
+                  />
+                </div>
+                ))
+              }
+            </Carousel>
+          </TabPanel>
+          <TabPanel value="counselling" className="p-0">
+            <Carousel animation="slide" swipe>
+            {
+                carouselImage?.images?.map((image, index) => (
+                  <div
+                  key={index}
+                  className="flex flex-row bg-red-900 rounded-lg h-[20vh] overflow-hidden"
+                >
+                  <img
+                    className="object-cover w-full"
+                    src={image?.toString()}
+                    alt="news picture"
+                  />
+                </div>
+                ))
+              }
+            </Carousel>
+          </TabPanel>
+          <Tabs
+            value={tab}
+            onChange={(event: React.SyntheticEvent, newValue: string) => {
+              setTab(newValue);
+            }}
+            textColor="primary"
+            variant="scrollable"
+            TabIndicatorProps={{
+              style: { backgroundColor: "red" },
+            }}
           >
-            Room {index + 1}
-          </Button>
-        ))}
-        <TextField
-          type="text"
-          className="bg-neutral-200 my-3 text-neutral-800 rounded-md w-10/12"
-          placeholder="Short description (optional)"
-        />
-        <Typography
-          variant="inherit"
-          className="text-neutral-50 font-semibold my-1"
-        >
-          Select Date
+            <Tab
+              label="Account"
+              value="account"
+              className="text-neutral-50 text-[0.8rem]"
+            />
+            <Tab
+              label="Library"
+              value="library"
+              className="text-neutral-50 text-[0.8rem]"
+            />
+            <Tab
+              label="Counselling"
+              value="counselling"
+              className="text-neutral-50 text-[0.8rem]"
+            />
+          </Tabs>
+          <TabPanel value="account" className="p-0">
+            <BookingTabContent rooms={account.children} currentRoom={currentRoom} setCurrentRoom={setCurrentRoom} />
+          </TabPanel>
+          <TabPanel value="library" className="p-0">
+            <BookingTabContent rooms={library.children} currentRoom={currentRoom} setCurrentRoom={setCurrentRoom}/>
+          </TabPanel>
+          <TabPanel value="counselling" className="p-0">
+            <BookingTabContent rooms={counselling.children} currentRoom={currentRoom} setCurrentRoom={setCurrentRoom}/>
+          </TabPanel>
+        </div>
+      </TabContext>
+
+      <Typography variant="h6" className="my-2">
+        Booked roomsRoute
+      </Typography>
+      <div className="bg-neutral-800 text-center pt-0 rounded-lg py-4 shadow-md">
+        <Typography className="text-neutral-50 p-4 text-left">
+          Booked roomsRoute will be listed here
         </Typography>
-        <input
-          type="date"
-          name=""
-          id=""
-          className="bg-neutral-200 text-neutral-800 rounded-md px-1 mx-1"
-        />
-        <button
-          type="submit"
-          // onClick={bookNow}
-          className="bg-red-600 text-white px-2 rounded-md mx-1"
-        >
-          Book Now
-        </button>
+        {/* {
+          bookings.map((booking, index) => (
+
+          ))
+        } */}
       </div>
-      <Typography variant="h6" className="my-2">Booked Rooms</Typography>
     </>
   );
 };
---
+++ src/pages/Campus/Booking/BookingTabContent.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useLocation } from "react-router-dom";
+import { Typography, Button } from "@mui/material";
+import { RoomBookingRouteType } from "../../../routes/config";
+
+type BookingTabContentProps = {
+  rooms?: RoomBookingRouteType["children"];
+  currentRoom: string | undefined
+  setCurrentRoom: React.Dispatch<React.SetStateAction<string | undefined>>
+};
+
+const BookingTabContent = (props: BookingTabContentProps) => {
+
+  return (
+    <>
+      <p className="text-neutral-50 font-semibold my-2">Select a Room</p>
+      {props.rooms?.map((room) => (
+        <Button
+          key={room.id}
+          onClick={() => props.setCurrentRoom(room.id)}
+          className={`${props.currentRoom == room.id ? 'bg-red-500' : 'bg-neutral-200'} rounded-md px-2 text-sm mx-1 text-neutral-800`}
+        >
+          {room.name}
+        </Button>
+      ))}
+      <input
+        type="text"
+        className="bg-neutral-200 my-3 p-2 text-neutral-800 rounded-md w-10/12"
+        placeholder="Short description (optional)"
+      />
+      <Typography
+        variant="inherit"
+        className="text-neutral-50 font-semibold my-1"
+      >
+        Select Date
+      </Typography>
+      <input
+        type="date"
+        name=""
+        // onClick={(date) => }
+        className="bg-neutral-200 text-neutral-800 rounded-md px-1 mx-1"
+      />
+      <button
+        type="submit"
+        // onClick={props.handleBooking}
+        className="bg-red-600 text-white px-2 rounded-md mx-1"
+      >
+        Book Now
+      </button>
+    </>
+  );
+};
+
+export default BookingTabContent;
